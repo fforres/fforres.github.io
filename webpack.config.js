60706972cf68e86109e410b8ce728add7f792754
felipe.torressepulveda@gmail.com <felipe torres>
2015-07-06 22:47:00 -0400

    adding first development ready version

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,50 @@
 'use strict';
 var webpack = require('webpack'),
-   path = require('path');
+    path = require('path');
 // PATHS
 var PATHS = {
-   app: __dirname + '/app',
-   bower: __dirname + '/app/bower_components'
+    app: __dirname + '',
 };
 module.exports = {
-   // config goes here
-   context: PATHS.app,
-   entry: {
-      app: './index.js'
-   },
-   devtool: 'eval',
-   output: {
-      path: PATHS.app,
-      filename: 'build/bundle.js',
-      pathinfo: true
-   },
-   resolve: {
-      modulesDirectories: ['node_modules']
-   },
-   module: {
-      loaders: [{
-         test: /\.css$/,
-         loader: "style-loader!css-loader"
-      }, {
-         test: /\.scss$/,
-         loader: "style!css!sass"
-      }, {
-         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-         loader: 'url-loader?limit=10000&minetype=application/font-woff'
-      }, {
-         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-         loader: 'file-loader'
-      }]
-   }
+    // config goes here
+    context: PATHS.app,
+    entry: {
+        app: './index.js'
+    },
+    devtool: 'eval',
+    output: {
+        path: PATHS.app,
+        filename: 'build/bundle.js',
+        pathinfo: true
+    },
+    resolve: {
+        modulesDirectories: ['node_modules']
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }, {
+            test: /\.scss$/,
+            loader: "style!css!sass"
+        }, {
+            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'url-loader?limit=10000&minetype=application/font-woff'
+        }, {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'file-loader'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: {
+                except: ['$super', '$', 'exports', 'require']
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true
+        })
+
+    ]
 };
