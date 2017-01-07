846be11a7af6cc8074f601c0931927690456e7b8
fetorres <fetorres@groupon.com>
2017-01-07 15:53:25 -0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: './src/entry.js',
   output: {
-    path: __dirname,
+    path: __dirname + '/dist',
     filename: 'bundle.js',
   },
   resolve: {
