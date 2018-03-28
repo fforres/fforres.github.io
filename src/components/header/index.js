4ad714f95c63bc1fc609e121dd28760bff055bdf
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2018-03-28 12:14:59 -0700

    adding logos

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -31,7 +31,58 @@ export class TopBar extends Component {
   render() {
     return (
       <div className={ classNames(layoutStyle.sidebar, style.headerStyle) }>
+        <span />
         <nav className={ style.nav }>{ this.renderLinks() }</nav>
+        <div className={ style.iconscontainer }>
+          <a
+            className={ style.link }
+            href="http://twitter.com/fforres"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <span className={ 'icon-twitter-square' } />
+          </a>
+          <a
+            className={ style.link }
+            href="http://facebook.com/fforres"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <span className={ 'icon-facebook-square' } />
+          </a>
+          <a
+            className={ style.link }
+            href="https://cl.linkedin.com/in/fforres"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <span className={ 'icon-linkedin-square' } />
+          </a>
+          <a
+            className={ style.link }
+            href="http://github.com/fforres"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <span className={ 'icon-github-square' } />
+          </a>
+          <a
+            className={ style.link }
+            href="http://flickr.com/fforres"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <span className={ 'icon-flickr' } />
+          </a>
+          <a
+            className={ style.link }
+            href="mailto:felipe.torressepulveda@gmail.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <span className={ 'icon-envelope-square' } />
+          </a>
+        </div>
       </div>
     )
   }
