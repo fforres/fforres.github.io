a81c272280d179a0e1f0bf73db625f2890c2b202
fforres <felipe.torressepulveda@gmail.com>
2019-01-13 16:14:26 -0800

    Adding typescript

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const withTypescript = require('@zeit/next-typescript')
+
+module.exports = withTypescript({
+  webpackDevMiddleware(config) {
+    config.watchOptions = {
+      ignored: [
+        /\.git\//,
+        /\.next\//,
+        /node_modules/
+      ]
+    }
+    return config;
+  }
+})
---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+
+// ./pages/_document.js
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <style>
+          {`
+            html { box-sizing: border-box; }
+            *, *:before, *:after { box-sizing: inherit; }
+            body, html { margin: 0; padding: 0; } /* custom! */
+          `}
+          </style>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
