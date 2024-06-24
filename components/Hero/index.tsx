3ae46b1235ff8d135d80bc0fa9b911a77d5ec705
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2024-06-23 20:43:54 -0700

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -8,7 +8,7 @@ class Hero extends Component {
       <Fragment>
         <section>
           <div className="backgroundImage" />
-          <div className="text">I do JavaScript stuff</div>
+          <div className="text">Hi! <br/> I'm fforres</div>
         </section>
         <style jsx>
           {`
